a0e667e6baf4b927fbdd4d5d612121edf2dcfdf5
ilgiz <keybor3@gmail.com>
2023-05-16 19:14:11 +0600

    refactor: restyle bottom_navigation_bar

--- lib/core/router/nav_router.dart
+++ lib/core/router/nav_router.dart
@@ -19,26 +19,75 @@ class NavRouter extends StatelessWidget {
         final tabRouter = AutoTabsRouter.of(context);
         return Scaffold(
           body: child,
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: tabRouter.activeIndex,
-            onTap: (index) => tabRouter.setActiveIndex(index),
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.chat_bubble),
-                label: "Chat",
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1), // Adjust color opacity as needed
+                  spreadRadius: 0.1,
+                  blurRadius: 10,
+                  offset: Offset(0, -3), // Move to top vertically
+                )
+              ],
+                borderRadius: BorderRadius.circular(24),
+            ),
+            height: 70, // Set the desired height
+
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(24),
+              child: BottomNavigationBar(
+                type: BottomNavigationBarType.shifting,
+
+                currentIndex: tabRouter.activeIndex,
+                onTap: (index) => tabRouter.setActiveIndex(index),
+                unselectedFontSize: 0,
+                selectedFontSize: 0,
+                items:  [
+                  BottomNavigationBarItem(
+                    icon: CustomBottomNavigationBarItem(label: "Chat",iconData:CupertinoIcons.chat_bubble,isSelected: tabRouter.activeIndex==0),
+                    label: "Chat",
+                  ),
+                  BottomNavigationBarItem(
+
+                    icon: CustomBottomNavigationBarItem(label: "Home",iconData:CupertinoIcons.home,isSelected: tabRouter.activeIndex==1),
+                    label: "Home",
+                  ),
+                  BottomNavigationBarItem(
+
+                    icon: CustomBottomNavigationBarItem(label: "Profile",iconData:CupertinoIcons.person,isSelected: tabRouter.activeIndex==2),
+                    label: "Profile",
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.person_fill),
-                label: "Profile",
-              ),
-            ],
+            ),
           ),
         );
       },
+    );
+  }
+}class CustomBottomNavigationBarItem extends StatelessWidget {
+  const CustomBottomNavigationBarItem({Key? key, required this.label, required this.isSelected, required this.iconData}) : super(key: key);
+  final IconData iconData;
+  final String label;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 4),
+      decoration: BoxDecoration(                  color: isSelected?Color(0xff4D8AF0).withOpacity(0.28):Colors.transparent,
+          borderRadius: BorderRadius.circular(16)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(iconData,color: isSelected?Color(0xff4D8AF0):Color(0xff161C2B),),
+          if(isSelected)...[
+            const SizedBox(width: 4,),
+            Text(label,style: TextStyle(color: Color(0xff4D8AF0),))
+          ]
+
+        ],
+      ),
     );
   }
 }
